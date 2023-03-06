6c188291366710bad17105197ad66d03812dea76
유남욱 <xeph123@naver.com>
2023-03-07 02:43:33 +0900

    2023-03-07 유남욱 세션.. @SessionAttributes{"user","tank"}

--- Insogae/src/main/java/com/smhrd/controller/CompanyController.java
+++ Insogae/src/main/java/com/smhrd/controller/CompanyController.java
@@ -1,69 +1,111 @@
 package com.smhrd.controller;
 
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
-@SessionAttributes("user")
+import com.smhrd.entity.Company;
+import com.smhrd.entity.TankData;
+import com.smhrd.mapper.CompanyMapper;
+import com.smhrd.mapper.TankMapper;
+
+@SessionAttributes({"user","tank"})
 @Controller
 public class CompanyController {
-	
+
+	@Autowired
+	CompanyMapper mapper;
+	@Autowired
+	TankMapper tankMapper;
+
 	@RequestMapping("/goLogin.do")
 	public String goLogin() {
 		return "redirect:/"; // 로그인 페이지 리다이렉트 이동
 	}
+
 	@RequestMapping("/")
 	public String login() {
 		return "login"; // 로그인 페이지로 포워드 이동 http://localhost:8882/login
 	}
+
 	@RequestMapping("/goJoin.do")
 	public String goJoin() {
 		return "redirect:/join"; // 회원가입페이지 리다이렉트 이동
 	}
+
 	@RequestMapping("/join")
 	public String join() {
 		return "join"; // 회원가입페이지 포워드 이동 http://localhost:8882/join
 	}
-	@RequestMapping("/goMain.do")
-	public String goMain() {
-		return "redirect:/main"; // 메인페이지(양식장 모니터링) 리다이렉트 이동
+
+	@RequestMapping("/goMain.do") 
+	public String goMain(HttpServletRequest request) {
+		return "redirect:/main"; // 메인페이지(양식장 모니터링) 리다이렉트 이동 
 	}
+	
 	@RequestMapping("/main")
 	public String main() {
-		return "main";  // 메인페이지(양식장 모니터링) 포워드이동 http://localhost:8882/main
+		
+		return "main"; // 메인페이지(양식장 모니터링) 포워드이동 http://localhost:8882/main
 	}
+
+	@RequestMapping("/goMonitoring.do")
+	public String goMonitoring() {
+		return "redirect:/monitoring"; // 메인페이지(양식장 모니터링) 리다이렉트 이동
+	}
+
+	@RequestMapping("/monitoring")
+	public String monitoring() {
+		return "monitoring"; // 메인페이지(양식장 모니터링) 포워드이동 http://localhost:8882/main
+	}
+
 	@RequestMapping("/goPredict.do")
 	public String goPredict() {
-		return "redirect:/predict";// ->> 수질 예측 리다이렉트 이동 
+		return "redirect:/predict";// ->> 수질 예측 리다이렉트 이동
 	}
+
 	@RequestMapping("/predict")
 	public String predict() {
 		return "predict";// ->> 수질 예측 포워드 이동 http://localhost:8882/predict
-	}	
+	}
+
 	@RequestMapping("/goSetting.do")
 	public String goSetting() {
-		return "redirect:/setting";// ->> 환경 설정 리다이렉트 이동 
+		return "redirect:/setting";// ->> 환경 설정 리다이렉트 이동
 	}
+
 	@RequestMapping("/setting")
 	public String setting() {
 		return "setting";// ->> 환경 설정 포워드 이동 http://localhost:8882/predict
-	}	
+	}
+
 	@RequestMapping("/goWaterguard.do")
 	public String goWaterguard() {
 		return "redirect:/waterguard"; // 메인페이지(양식장 모니터링) 리다이렉트 이동
 	}
+
 	@RequestMapping("/waterguard")
 	public String waterguard() {
-		return "waterguard";  // 메인페이지(양식장 모니터링) 포워드이동 http://localhost:8882/main
+		return "waterguard"; // 메인페이지(양식장 모니터링) 포워드이동 http://localhost:8882/main
 	}
+
 	@RequestMapping("/logout.do")
 	public String logout(SessionStatus session) {
 		session.setComplete();
 		return "redirect:/"; // 로그아웃 및 세션종료
 	}
-	
+
 	@RequestMapping("/gograph")
 	public String goGraph() {
 		return "graph";
--- Insogae/src/main/resources/static/js/predict.js
+++ Insogae/src/main/resources/static/js/predict.js
@@ -1,13 +1,4 @@
-/*const ondoArray[i] = document.getElementById('ondo-gp-1');
-const doArray[i] = document.getElementById('do-gp-1');
-const phArray[i] = document.getElementById('ph-gp-1');
-const saltArray[i] = document.getElementById('salt-gp-1');
-const contentArray[i] = document.getElementById('report-view-1');
-let dataArray[i] = "온도";
-const dataList = ['온도','DO','pH','염분']
-const reportArray = ['#report-1','#report-2','#report-3','#report-4','#report-5','#report-6','#report-7','#report-8']
-*/
-let num = 8;
+let num = 8; // 수조 개수 설정
 
 const valueArray = []
 const reportArray = []
@@ -19,7 +10,8 @@ const contentArray = []
 const rectangleArray = []
 const imgtagArray = []
 let dataArray = []
-for (let i = 1; i <= num; i++)  {
+
+for (let i = 1; i <= num; i++) {
 	valueArray.push('.value-' + i);
 	reportArray.push('#report-' + i);
 	ondoArray.push('#ondo-gp-' + i);
@@ -31,138 +23,71 @@ for (let i = 1; i <= num; i++)  {
 	imgtagArray.push('#imgtag-' + i);
 	dataArray.push('온도');
 }
+
 for (let i = 0; i < num; i++) {
-	$(reportArray[i]).on('click', function() {
+	$(reportArray[i]).on('click', function () {
 		$(contentArray[i]).show();
 		let tempArray = $(rectangleArray[i]);
 		let tempArray2 = $(valueArray[i]);
 		console.log(tempArray);
 		console.log(tempArray2);
-		for (let j = 0;j<4;j++)  {
-			if (tempArray2[j].innerText = dataArray[i])  {
-				document.getElementById(tempArray[j].id).style.display="none";
+		for (let j = 0; j < 4; j++) {
+			if (tempArray2[j].innerText == dataArray[i]) {
+				document.getElementById(tempArray[j].id).style.display = "none";
 			}
 		}
 	});
 }
+
 for (let i = 0; i < num; i++) {
-	$(imgtagArray[i]).on('click', function(){
+	$(imgtagArray[i]).on('click', function () {
 		$(contentArray[i]).hide();
 		let tempArray = $(rectangleArray[i]);
 		let tempArray2 = $(valueArray[i]);
-		for (let j=0; j < 4; j++) {
-			if (tempArray2[j].innerText  == dataArray[i]) {
+		for (let j = 0; j < 4; j++) {
+			if (tempArray2[j].innerText == dataArray[i]) {
 				console.log(tempArray[j]);
-				document.getElementById(tempArray[j].id).style.display="block";
+				document.getElementById(tempArray[j].id).style.display = "block";
 			}
 		}
 	});
 }
 
-for(let i=0; i<num; i++){
-$(valueArray[i]).on('click',  function() {
-	dataArray[i] = $(this).text();
-	console.log(dataArray[i])
+for (let i = 0; i < num; i++) {
+	$(valueArray[i]).on('click', function () {
+		dataArray[i] = $(this).text();
+		console.log(dataArray[i])
 
-	if (dataArray[i] ==  '온도') {
-		$(ondoArray[i]).show();
-		$(doArray[i]).hide();
-		$(phArray[i]).hide();
-		$(saltArray[i]).hide();
-		$(contentArray[i]).hide();
-	}
-	else if (dataArray[i] ==  'pH') {
-	$(ondoArray[i]).hide();
-	$(doArray[i]).hide();
-	$(phArray[i]).show();
-	$(saltArray[i]).hide();
-	$(contentArray[i]).hide();
-}
-else if  (dataArray[i] ==  'DO') {
-	$(ondoArray[i]).hide();
-	$(doArray[i]).show();
-	$(phArray[i]).hide();
-	$(saltArray[i]).hide();
-	$(contentArray[i]).hide();
-}
-	else if  (dataArray[i]=='염분') {
-	$(ondoArray[i]).hide();
-	$(doArray[i]).hide();
-	$(phArray[i]).hide();
-	$(saltArray[i]).show();
-	$(contentArray[i]).hide();
-}
-});
-}
-/*
-const ondo2 = document.getElementById('ondo-gp-2');
-const do2 = document.getElementById('do-gp-2');
-const ph2 = document.getElementById('ph-gp-2');
-const salt2 = document.getElementById('salt-gp-2');
-const content2 = document.getElementById('report-view-2');
-let data2 = "온도";
-$(reportArray[1]).on('click',function(){
-	content2.style.display = 'block';
-	let tempArray = $('.rectangle-gp-2');
-	let tempArray2 = $('.value-2');
-	console.log(tempArray);
-	
-	for(let i=0;i<4;i++){
-		if(tempArray2[i].innerText==data2){
-		console.log(tempArray[i].id)
-		document.getElementById(tempArray[i].id).style.display="none";
+		if (dataArray[i] == '온도') {
+			$(ondoArray[i]).show();
+			$(doArray[i]).hide();
+			$(phArray[i]).hide();
+			$(saltArray[i]).hide();
+			$(contentArray[i]).hide();
 		}
-	}
-});
-	
-$('#imgtag-2').on('click',function(){
-	content2.style.display = 'none';
-	let tempArray = $('.rectangle-gp-2');
-	let tempArray2 = $('.value-2');
-	for(let i=0;i<4;i++){
-		if(tempArray2[i].innerText==data2){
-		document.getElementById(tempArray[i].id).style.display="block";
+		else if (dataArray[i] == 'pH') {
+			$(ondoArray[i]).hide();
+			$(doArray[i]).hide();
+			$(phArray[i]).show();
+			$(saltArray[i]).hide();
+			$(contentArray[i]).hide();
+		} else if (dataArray[i] == 'DO') {
+			$(ondoArray[i]).hide();
+			$(doArray[i]).show();
+			$(phArray[i]).hide();
+			$(saltArray[i]).hide();
+			$(contentArray[i]).hide();
 		}
-	}
-	
-});
-	
-	
-$('.value-2').on('click',function(){
-	data2 = $(this).text();
-	console.log(data2)
-	
-	if(data2=='온도'){
-		ondo2.style.display = 'block';
-			do2.style.display = 'none';
-			ph2.style.display = 'none';
-			salt2.style.display = 'none';
-			content2.style.display = 'none';
-	}
-	else if(data2=='pH'){
-		ondo2.style.display = 'none';
-			do2.style.display = 'none';
-			ph2.style.display = 'block';
-			salt2.style.display = 'none';
-			content2.style.display = 'none';
-	}
-	else if(data2=='DO'){
-		ondo2.style.display = 'none';
-			do2.style.display = 'block';
-			ph2.style.display = 'none';
-			salt2.style.display = 'none';
-			content2.style.display = 'none';
-	}
-	else if(data2=='염분'){
-		ondo2.style.display = 'none';
-			do2.style.display = 'none';
-			ph2.style.display = 'none';
-			salt2.style.display = 'block';
-			content2.style.display = 'none';
-	}
-});
-	
-*/
+		else if (dataArray[i] == '염분') {
+			$(ondoArray[i]).hide();
+			$(doArray[i]).hide();
+			$(phArray[i]).hide();
+			$(saltArray[i]).show();
+			$(contentArray[i]).hide();
+		}
+	});
+}
+
 
 const myButton = document.getElementById("next-page");
 myButton.addEventListener('click', e => {
@@ -171,5 +96,5 @@ myButton.addEventListener('click', e => {
 
 const myButton2 = document.getElementById("before-page");
 myButton2.addEventListener('click', e => {
-	document.getElementById("container").scrollIntoView({  behavior: 'smooth', block: "nearest", inline: "start" });
+	document.getElementById("container").scrollIntoView({ behavior: 'smooth', block: "nearest", inline: "start" });
 });
