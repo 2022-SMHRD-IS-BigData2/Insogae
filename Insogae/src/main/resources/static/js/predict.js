26cb4cd6fde4be09320f2114ba0ecd749034b337
ddfdf <ddfdf@DESKTOP-O6533HN>
2023-03-07 10:20:36 +0900

    23.03.07 박주빈 모니터링/수질예측 실시간 시계, 리포트 버튼 위치 수정

--- Insogae/src/main/resources/static/js/predict.js
+++ Insogae/src/main/resources/static/js/predict.js
@@ -98,3 +98,32 @@ const myButton2 = document.getElementById("before-page");
 myButton2.addEventListener('click', e => {
 	document.getElementById("container").scrollIntoView({ behavior: 'smooth', block: "nearest", inline: "start" });
 });
+
+
+
+function currentTime() {
+  const date = new Date(); 
+  let yyyy = date.getFullYear();
+  let MM = date.getMonth()+1;
+  let dd = date.getDate();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let session = "AM";
+
+  if (hh > 12) {
+    session = "PM";
+  }
+
+  hh = (hh < 10) ? "0" + hh : hh;
+  mm = (mm < 10) ? "0" + mm : mm;
+  ss = (ss < 10) ? "0" + ss : ss;
+
+  const time = yyyy + "년 " + MM + "월 " + dd + "일 " + session + " "  + hh + ":" + mm;
+
+  document.getElementById("clock").innerText = time;
+
+  setTimeout(() => currentTime(), 1000);
+}
+
+currentTime();
