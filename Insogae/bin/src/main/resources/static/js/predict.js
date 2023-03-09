b7ebf668254accb10a1b63be742436fb879b4684
smhrd <smhrd@DESKTOP-UGE4P6G>
2023-03-09 15:46:44 +0900

    모니터링 현재시간을 DB에 있는 데이터로 변경 -이우철

--- Insogae/bin/src/main/resources/static/js/predict.js
+++ Insogae/bin/src/main/resources/static/js/predict.js
@@ -1,190 +1,140 @@
-const ondo2 = document.getElementById('ondo-gp-1');
-const do2 = document.getElementById('do-gp-1');
-const ph2 = document.getElementById('ph-gp-1');
-const salt2 = document.getElementById('salt-gp-1');
-const content2 = document.getElementById('report-view-1');
-let data = "온도";
+let num = 8; // 수조 개수 설정
 
-$('.report').on('click',function(){
-	content2.style.display = 'block';
-	console.log(data)
+const valueArray = []
+const reportArray = []
+const ondoArray = []
+const doArray = []
+const phArray = []
+const saltArray = []
+const contentArray = []
+const rectangleArray = []
+const imgtagArray = []
+let dataArray = []
+
+for (let i = 1; i <= num; i++) {
+   valueArray.push('.value-' + i);
+   reportArray.push('#report-' + i);
+   ondoArray.push('#ondo-gp-' + i);
+   doArray.push('#do-gp-' + i);
+   phArray.push('#ph-gp-' + i);
+   saltArray.push('#salt-gp-' + i);
+   contentArray.push('#report-view-' + i);
+   rectangleArray.push('.rectangle-gp-' + i);
+   imgtagArray.push('#imgtag-' + i);
+   dataArray.push('온도');
+}
+
+
+
+for (let i = 0; i < num; i++) {
+   $(reportArray[i]).on('click', function () {
+      $(contentArray[i]).show();
+      let tempArray = $(rectangleArray[i]);
+      let tempArray2 = $(valueArray[i]);
+      console.log(tempArray);
+      console.log(tempArray2);
+      for (let j = 0; j < 4; j++) {
+         if (tempArray2[j].innerText == dataArray[i]) {
+            document.getElementById(tempArray[j].id).style.display = "none";
+         }
+      }
+   });
+}
+
+for (let i = 0; i < num; i++) {
+   $(imgtagArray[i]).on('click', function () {
+      $(contentArray[i]).hide();
+      let tempArray = $(rectangleArray[i]);
+      let tempArray2 = $(valueArray[i]);
+      for (let j = 0; j < 4; j++) {
+         if (tempArray2[j].innerText == dataArray[i]) {
+            console.log(tempArray[j]);
+            document.getElementById(tempArray[j].id).style.display = "block";
+         }
+      }
+   });
+}
+
+for (let i = 0; i < num; i++) {
+	$(ondoArray[i]).show();
+	$(doArray[i]).hide();
+	$(phArray[i]).hide();
+	$(saltArray[i]).hide();
+	$(contentArray[i]).hide();
 	
-	let testArray = $('.rectangle-value');
-	console.log(testArray);
-	
-	for(let i=0;i<testArray.lenght;i++){
-		if(testArray[i].innerText==data){
-		console.log("testDAta "+ testArray[i].innerText)
-			testArray[i].style.display='none'
-		}
-	}
-	
-});
-$('#imgtag').on('click',function(){
-	content2.style.display = 'none';
-	console.log(data)
-	
-	let testArray = $('.rectangle-value');
-	console.log(testArray)
-	for(let i=0;i<testArray.lenght;i++){
-		if(testArray[i].innerText==data){
-		console.log("testDAta "+ testArray[i].innerText)
-			testArray[i].style.display='block'
-		}
-		
-	}
-	
-});
+   $(valueArray[i]).on('click', function () {
+      dataArray[i] = $(this).text();
+      console.log(dataArray[i])
 
-
-$('.rectangle-value').on('click',function(){
-	data = $(this).text();
-	console.log(data)
-	
-	if(data=='온도'){
-		ondo2.style.display = 'block';
-  		do2.style.display = 'none';
-  		ph2.style.display = 'none';
-  		salt2.style.display = 'none';
-	}
-	else if(data=='ph'){
-		ondo2.style.display = 'none';
-  		do2.style.display = 'none';
-  		ph2.style.display = 'block';
-  		salt2.style.display = 'none';
-	}
-	else if(data=='do'){
-		ondo2.style.display = 'none';
-  		do2.style.display = 'block';
-  		ph2.style.display = 'none';
-  		salt2.style.display = 'none';
-	}
-	else if(data=='염분'){
-		ondo2.style.display = 'none';
-  		do2.style.display = 'none';
-  		ph2.style.display = 'none';
-  		salt2.style.display = 'block';
-	}
-});
-
-
-function showReport1(){
-    // 토글 할 버튼 선택 (btn1)
-  const content1 = document.getElementById('report-view-1');
-  const ondo1 = document.getElementById('ondo-gp-1');
-  const do1 = document.getElementById('do-gp-1');
-  const ph1 = document.getElementById('ph-gp-1');
-  const salt1 = document.getElementById('salt-gp-1');
-  content1.style.display = 'block';
-  ondo1.style.display = 'none';
-  do1.style.display = 'none';
-  ph1.style.display = 'none';
-  salt1.style.display = 'none';
+      if (dataArray[i] == '온도') {
+         $(ondoArray[i]).show();
+         $(doArray[i]).hide();
+         $(phArray[i]).hide();
+         $(saltArray[i]).hide();
+         $(contentArray[i]).hide();
+      }
+      else if (dataArray[i] == 'pH') {
+         
+         console.log($(ondoArray[i]))
+         $(ondoArray[i]).hide();
+         $(doArray[i]).hide();
+         $(phArray[i]).show();
+         $(saltArray[i]).hide();
+         $(contentArray[i]).hide();
+      } else if (dataArray[i] == 'DO') {
+         $(ondoArray[i]).hide();
+         $(doArray[i]).show();
+         $(phArray[i]).hide();
+         $(saltArray[i]).hide();
+         $(contentArray[i]).hide();
+      }
+      else if (dataArray[i] == '염분') {
+         $(ondoArray[i]).hide();
+         $(doArray[i]).hide();
+         $(phArray[i]).hide();
+         $(saltArray[i]).show();
+         $(contentArray[i]).hide();
+      }
+   });
 }
-function hideReport1(){
-  const content1 = document.getElementById('report-view-1');
-  const ondo1 = document.getElementById('ondo-gp-1');
-  content1.style.display = 'none';
-  ondo1.style.display = 'block';
-}
-
-function showDo1(){
-	const content1 = document.getElementById('report-view-1');
-	const do1 = document.getElementById('do-gp-1');
-	const ondo1 = document.getElementById('do-gp-1');
-	const ph1 = document.getElementById('ph-gp-1');
-	const salt1 = document.getElementById('salt-gp-1');
-	do1.style.display='block';
-	ondo1.style.display='none';
-	ph1.style.display='none';
-	salt1.style.display='none';
-	content1.style.display = 'none';
-}
-function showReport2(){
-    // 토글 할 버튼 선택 (btn1)
-  const content1 = document.getElementById('report-view-2');
-  content1.style.display = 'block';
-}
-function hideReport2(){
-  const content1 = document.getElementById('report-view-2');
-  content1.style.display = 'none';
-}
-
-
-function showReport3(){
-      // 토글 할 버튼 선택 (btn1)
-    const content1 = document.getElementById('report-view-3');
-    content1.style.display = 'block';
-}
-function hideReport3(){
-    const content1 = document.getElementById('report-view-3');
-    content1.style.display = 'none';
-}
-
-
-function showReport4(){
-    // 토글 할 버튼 선택 (btn1)
-  const content1 = document.getElementById('report-view-4');
-  content1.style.display = 'block';
-}
-function hideReport4(){
-  const content1 = document.getElementById('report-view-4');
-  content1.style.display = 'none';
-}
-
-function showReport5(){
-  // 토글 할 버튼 선택 (btn1)
-const content1 = document.getElementById('report-view-5');
-content1.style.display = 'block';
-}
-function hideReport5(){
-const content1 = document.getElementById('report-view-5');
-content1.style.display = 'none';
-}
-
-function showReport6(){
-  // 토글 할 버튼 선택 (btn1)
-const content1 = document.getElementById('report-view-6');
-content1.style.display = 'block';
-}
-function hideReport6(){
-const content1 = document.getElementById('report-view-6');
-content1.style.display = 'none';
-}
-
-function showReport7(){
-  // 토글 할 버튼 선택 (btn1)
-const content1 = document.getElementById('report-view-7');
-content1.style.display = 'block';
-}
-function hideReport7(){
-const content1 = document.getElementById('report-view-7');
-content1.style.display = 'none';
-}
-
-function showReport8(){
-  // 토글 할 버튼 선택 (btn1)
-const content1 = document.getElementById('report-view-8');
-content1.style.display = 'block';
-}
-function hideReport8(){
-const content1 = document.getElementById('report-view-8');
-content1.style.display = 'none';
-}
-
-
-
 
 
 const myButton = document.getElementById("next-page");
-;
-    myButton.addEventListener('click', e=> {
-      document.getElementById("container").scrollIntoView({behavior:'smooth', block: "nearest", inline: "end"});
-    });
+myButton.addEventListener('click', () => {
+   document.getElementById("container").scrollIntoView({ behavior: 'smooth', block: "nearest", inline: "end" });
+});
 
 const myButton2 = document.getElementById("before-page");
-;
-    myButton2.addEventListener('click', e=> {
-      document.getElementById("container").scrollIntoView({behavior:'smooth', block: "nearest", inline: "start"});
-    });    
-    
+myButton2.addEventListener('click', () => {
+   document.getElementById("container").scrollIntoView({ behavior: 'smooth', block: "nearest", inline: "start" });
+});
+
+
+function currentTime() {
+	const date = new Date();
+	let yyyy = date.getFullYear();
+	let MM = date.getMonth() + 1;
+	let dd = date.getDate();
+	let hh = date.getHours();
+	let mm = date.getMinutes();
+	let ss = date.getSeconds();
+	let session = "AM";
+
+	if (hh > 12) {
+		session = "PM";
+	}
+
+	hh = (hh < 10) ? "0" + hh : hh;
+	mm = (mm < 10) ? "0" + mm : mm;
+	ss = (ss < 10) ? "0" + ss : ss;
+
+	const time = yyyy + "년 " + MM + "월 " + dd + "일 " + session + " " + hh + ":" + mm;
+	
+	for(let i =0; i<$(".tctm").length;i++){
+		$(".tctm")[i].innerText = time;
+	};
+
+	setTimeout(() => currentTime(), 1000);
+};
+
+currentTime();
