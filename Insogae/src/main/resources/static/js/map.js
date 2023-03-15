939926e4ca9c9eb921f26849cffc5728228bb40d
smhrd <smhrd@DESKTOP-UGE4P6G>
2023-03-15 17:26:35 +0900

    일단 DB 연동해서 수조의 위치, 수조의 개수 연결했습니다. 나머진 공부를 해야.... -이우철

--- Insogae/src/main/resources/static/js/map.js
+++ Insogae/src/main/resources/static/js/map.js
@@ -48,16 +48,94 @@
       marker.description.forEach(desc => {
         content += `<h8 style = "color:black">${desc}</h8><br>`
       });
-      
-      
-      
-      
-      
-      
+  
+    
       infoWindow.setContent(content);
 
       // 정보창 열기
       infoWindow.open(map, mapMarker);
     });
   });
+}
+
+
+
+
+
+
+
+ function initMap() {
+
+$.ajax({
+	url : 'location_data',
+	data : {
+		COMPANY_ID : user
+	},
+	success : function(res){
+		
+		
+		  // 초기화 함수    
+   
+  // 지도 생성
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: { lat: 35.1595454, lng: 126.8526012 } // 광주
+  });
+
+  // 마커 데이터
+  const markerData = [
+    {
+      name: '수산기술연구소 수조',
+      lat: 35.9855015,
+      lng: 127.2620517,
+      description: ['수조위치:  '+res[0].location, '수조의 개수: '+res[0].count, '사육개시일: 2021-07-13', '온도: 28°C']
+    },
+    {
+      name: '한농대 수조',
+      lat: 35.8262965,
+      lng: 127.0656925,
+      description: ['수조위치:  '+res[1].location, '수조의 개수: '+res[1].count, '사육개시일: 2021-07-13', '온도: 31°C']
+    },
+    {
+      name: '어가 수조',
+      lat: 34.5029352,
+      lng: 126.3694397,
+      description: ['수조위치:  '+res[2].location, '수조의 개수: '+res[2].count, '사육개시일: 2021-07-13', '온도: 28°C']
+    }
+  ];
+
+  var myIcon = new google.maps.MarkerImage("./img/sh3.png", null, null, null, new google.maps.Size(50,50));
+
+  // 마커 생성
+  markerData.forEach(marker => {
+    const position = { lat: marker.lat, lng: marker.lng };
+    const mapMarker = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: marker.name,
+      icon:myIcon,
+    });
+
+    // 마커 클릭 이벤트 리스너 등록
+    mapMarker.addListener('click', () => {
+      // 정보창 생성
+      const infoWindow = new google.maps.InfoWindow();
+      let content = `<h6 style = "color:black">${marker.name}</h6><br>`;
+      marker.description.forEach(desc => {
+        content += `<h8 style = "color:black">${desc}</h8><br>`
+      });
+  
+    
+      infoWindow.setContent(content);
+
+      // 정보창 열기
+      infoWindow.open(map, mapMarker);
+    });
+  });
+
+	},
+	error : function(){
+		console.log('error!');
+	}
+})
 }
